58b63cc39efa96b5e7ab9be1ca5feb61525fb282
luketurner <contact@luketurner.org>
2012-03-22 16:54:58 -0700

    Replaced meaningless placeholder with some nice, useful notes.

--- animagic.py
+++ animagic.py
@@ -1,1 +1,6 @@
-print("ho!")
+# This file is probably going to have the following things:
+
+# Config file parser to interpret list of anime and its identifying characteristics
+# Nyaa.eu crawler (threaded?)
+# Torrent downloading thing. (threaded)
+# Some kind of thing to diff the anime episodes you already have, with out list from Nyaa.
